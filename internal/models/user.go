4fd0ae78071bb372426fcc5f602e294b25754963
Vladislav Travinskiy <114735742+TravikDev@users.noreply.github.com>
2025-07-17 11:20:45 -0800

    Implement account lockout after failed logins

--- internal/models/user.go
+++ internal/models/user.go
@@ -1,7 +1,9 @@
 package models
 
 type User struct {
-	ID           int    `json:"id"`
-	Username     string `json:"username"`
-	PasswordHash string `json:"-"`
+	ID             int    `json:"id"`
+	Username       string `json:"username"`
+	PasswordHash   string `json:"-"`
+	FailedAttempts int    `json:"-"`
+	Locked         bool   `json:"locked"`
 }
